cc89dbb990a5c8908dd152f7a71ac0a7ee8896ae
Andres Morocho <andresmorocho267@gmail.com>
2023-09-26 16:43:10 -0700

    Changed env variables inside functions folder

--- functions/index.js
+++ functions/index.js
@@ -9,13 +9,13 @@ admin.initializeApp();
 app.use(cors({ origin: true }));
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
 app.get("/getFirebaseConfig", (req, res) => {
